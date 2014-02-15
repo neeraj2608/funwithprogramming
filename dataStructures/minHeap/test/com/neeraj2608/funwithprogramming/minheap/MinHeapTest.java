046c8dbe486b1b1df43ebbec273ca12f53566abb
Raj Rao <neeraj2608@gmail.com>
2014-02-15 11:39:27 -0500

    Added unit tests for MinHeap. Added comments for trie.

--- dataStructures/minHeap/test/com/neeraj2608/funwithprogramming/minheap/MinHeapTest.java
+++ dataStructures/minHeap/test/com/neeraj2608/funwithprogramming/minheap/MinHeapTest.java
@@ -1,6 +1,8 @@
 package com.neeraj2608.funwithprogramming.minheap;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -28,6 +30,35 @@ public class MinHeapTest{
     minHeap.deleteMinSiftDown();
   }
   
-  //TODO: add tests for remaining methods
-
+  @Test
+  public void testIsEmpty(){
+    assertTrue(minHeap.isEmpty());
+    minHeap.insert(1);
+    assertFalse(minHeap.isEmpty());
+  }
+  
+  @Test
+  public void testDeleteMinSiftDown(){
+    minHeap.insert(5);
+    minHeap.insert(4);
+    minHeap.insert(7);
+    minHeap.insert(1);
+    assertEquals(1, minHeap.deleteMinSiftDown());
+    assertEquals(4, minHeap.deleteMinSiftDown());
+    assertEquals(5, minHeap.deleteMinSiftDown());
+    assertEquals(7, minHeap.deleteMinSiftDown());
+  }
+  
+  @Test
+  public void testDeleteMinSiftUp(){
+    minHeap.insert(5);
+    minHeap.insert(4);
+    minHeap.insert(7);
+    minHeap.insert(1);
+    assertEquals(1, minHeap.deleteMinSiftUp());
+    assertEquals(4, minHeap.deleteMinSiftUp());
+    assertEquals(5, minHeap.deleteMinSiftUp());
+    assertEquals(7, minHeap.deleteMinSiftUp());
+  }
+  
 }
